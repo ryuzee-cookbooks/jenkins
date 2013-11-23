ea66c845ba7b14f6d507cb7d5e45eee9abbd21b5
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-11-24 06:27:18 +0900

    clarify license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "jenkins"
 maintainer       "ryuzee"
 maintainer_email "ryuzee@gmail.com"
-license          "All rights reserved"
+license          "MIT"
 description      "Installs/Configures jenkins"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
