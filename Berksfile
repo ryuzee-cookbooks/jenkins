d37ca769dbb88d00c18110567b1d36a24be02f4a
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-11-07 17:54:56 +0900

    fix issue

--- Berksfile
+++ Berksfile
@@ -1,4 +1,6 @@
-site :opscode
+source "http://api.berkshelf.com"
 
 metadata
+
+cookbook 'apache2-simple', git:"https://github.com/ryuzee-cookbooks/apache2-simple.git"
 
--- recipes/http.rb
+++ recipes/http.rb
@@ -9,7 +9,7 @@
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
 
-include_recipe "apache2"
+include_recipe "apache2-simple"
 
 case node[:platform]
 when "ubuntu", "debian"
@@ -18,15 +18,23 @@ when "ubuntu", "debian"
   execute cmd do
     action :run
     only_if do
-      ! File.exists?("/etc/apache2/conf.d/jenkins.conf") 
+      ! File.exists?("/etc/apache2/conf-enabled/jenkins.conf") 
     end
   end
 
-  template "/etc/apache2/conf.d/jenkins.conf" do
+  template "/etc/apache2/conf-enabled/jenkins.conf" do
     source "jenkins.conf.erb"
     owner "root"
     group "root"
     mode 0644
     notifies :reload, 'service[apache2]'
+  end
+when "centos", "amazon"
+  template "/etc/httpd/conf.d/jenkins.conf" do
+    source "jenkins.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    notifies :reload, 'service[httpd]'
   end
 end
