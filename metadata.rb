32edb665d01561ea2b59796c16b6dbd6195d4426
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-11-29 09:46:15 +0900

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "ryuzee@gmail.com"
 license          "MIT"
 description      "Installs/Configures jenkins"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 depends          "yum"
 depends          "yum-epel"
 depends          "apt"
