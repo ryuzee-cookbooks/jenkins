9041f4459fd672b4aba9a50fcba21dec510f91cb
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-11-29 09:12:33 +0900

    fix metadata

--- metadata.rb
+++ metadata.rb
@@ -5,9 +5,8 @@ license          "MIT"
 description      "Installs/Configures jenkins"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.3"
-depends          "yum", ">= 3.0"
+depends          "yum"
 depends          "yum-epel"
 depends          "apt"
 depends          "ca-certificates"
 depends          "apache2-simple"
-
